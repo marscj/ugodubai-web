bbc05385ed5882a26ff19378aae3c8beaf3f7e3d
marscj <mars.jinxing@gmail.com>
2023-07-17 12:34:01 +0400

    commit

--- lib/app/modules/backend/console/dashboard/views/dashboard_view.dart
+++ lib/app/modules/backend/console/dashboard/views/dashboard_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ugodubai/app/extensions/widgets.dart';
 
 import '../controllers/dashboard_controller.dart';
 
